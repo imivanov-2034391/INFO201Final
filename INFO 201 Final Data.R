1ab16dfbe6ed60b2e751a6ca00390fd4d1b24479
ciins <ciinszhang@gmail.com>
2024-02-14 17:03:20 -0800

    change description

--- INFO 201 Final Data.R
+++ INFO 201 Final Data.R
@@ -1,2 +1,16 @@
+rm(list = ls())
+
+# library statements
+library(dplyr)
+library(stringr)
+library(testthat)
+
+# Loads in two data sets
 sleep_df <- read.csv("Sleep_health_and_lifestyle_dataset.csv")
 job_df <- read.csv("Occupation Employment and Wage Statistics.csv")
+
+# Data frame combine
+combined_df <- sleep_df %>%
+  left_join(job_df, by = c("Occupation" = "OCC_Title"))
+
+# Filter out N/A value which sleep might not have some jobs which wage has
