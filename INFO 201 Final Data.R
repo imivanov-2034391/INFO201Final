fffac655a8f0865765f5cb93320a8b572fca4122
ciins <ciinszhang@gmail.com>
2024-02-17 01:48:20 -0800

    Update INFO 201 Final Data.R

--- INFO 201 Final Data.R
+++ INFO 201 Final Data.R
@@ -13,7 +13,7 @@ library(testthat)
 # Loads in two data sets and the conversion chart
 sleep_df <- read.csv("Sleep_health_and_lifestyle_dataset.csv")
 job_df <- read.csv("Occupation Employment and Wage Statistics.csv")
-conversion_df <- read.csv("Occupation Conversion Chart 2.csv")
+conversion_df <- read.csv("Occupation Conversion Chart.csv")
 
 
 # Cleans job_df and saved as job_wages_df with only job title and wages columns
@@ -24,5 +24,4 @@ job_wages_df <- job_df %>% select(OCC_TITLE, H_MEAN, A_MEAN)
 combined_df <- sleep_df %>%
   left_join(conversion_df, by = c("Occupation" = "Occupation_sleep")) %>% 
   left_join(job_wages_df, by = c("Corresponding.Occupation_job" = "OCC_TITLE"), relationship = "many-to-many") %>% 
-  distinct(Person.ID, .keep_all = TRUE)
 
