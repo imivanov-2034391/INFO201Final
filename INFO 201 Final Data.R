706141fbfad0bc709062a43399435ce4920dec5d
ciins <ciinszhang@gmail.com>
2024-02-19 17:36:19 -0800

    update

--- INFO 201 Final Data.R
+++ INFO 201 Final Data.R
@@ -59,5 +59,5 @@ percentage_occupation <- combined_df %>%
 
 
 # Export unified and cleaned CSV dataset file
-write.csv(combined_df, "Sleep and Occupation Combined Dataset.csv")
+write.csv(combined_df, "Comprehensive Sleep Health and Occupational Statistics Dataset.csv")
   
