eb23feab8d179566c42b24338827285de47f4056
ciins <ciinszhang@gmail.com>
2024-03-04 20:17:11 +0800

    Update server.R

--- server.R
+++ server.R
@@ -17,4 +17,31 @@ server <- function(input, output){
   
   # TODO Make outputs based on the UI inputs here
   
+  
+  # Interactive Page 1
+  output$phy_occ_plot <- renderPlotly({
+    my_plot <- ggplot(combined_df) +
+      geom_point(mapping = aes(
+        x = Physical.Activity.Level,
+        y = Occupation,
+        color = Physical.Activity.Level
+      ))
+    return(ggplotly(my_plot))
+  })
+  
+  
+  # Interactive Page 2
+  
+  
+  
+  
+  
+  
+  # Interactive Page 3
+  
+  
+  
+  
+  
+  
 }
