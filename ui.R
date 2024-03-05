08caa093cf054e48f8835e72b5f6a8ccca03dcb0
ciins <ciinszhang@gmail.com>
2024-03-05 12:08:22 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -19,14 +19,8 @@ my_theme <- bs_theme(bg = "white", #background
 ## OVERVIEW TAB INFO
 
 overview_tab <- tabPanel("Introduction",
-<<<<<<< HEAD
    tags$b("BH-5 Group Members:"), p("Sohyun Han, Sera Lee, Ivette Immanov, Lele Zhang"),
-=======
    h1("The Impact of Occupation on Physical Health"),
-   h4("INFO 201 BH-5 Final Project"),
-   tags$b("Group Members:"), p("Sohyun Han, Sera Lee, Ivette Immanov, Lele Zhang"),
-   p("March 6th, 2024"),
->>>>>>> 72354a4788255100e838ecff18f5324f21ed5c65
    h3("Introduction"),
    
    img(src = 'https://cdn.pixabay.com/photo/2017/03/04/13/12/alarm-clock-2116007_1280.jpg', height="40%", width="40%", align="right"),
